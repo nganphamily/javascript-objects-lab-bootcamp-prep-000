34f86fdb2fdbfffcfbb08ada3cb092ffb7189e6a
NGAN PHAM <nganvpham@gmail.com>
2018-01-26 04:49:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-   Object.assign(object, {[key]:value});
+   Object.assign(object, {key:value});
    return object;
 }
