928d040ffc08937a75f5116386cb8b7ac96630b9
NGAN PHAM <nganvpham@gmail.com>
2018-01-26 05:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-   delete object[key];
-   return object; 
+   return delete object[key];
 }
